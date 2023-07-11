61467565f7d2c9f7861743f8705b0f5ef69198ce
Allenyou <allenyou1126@gmail.com>
2023-07-11 11:18:00 +0800

    first commit

---
+++ lsky-pro-wordpress.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Lsky Pro Wordpress
+* Plugin URI: https://www.allenyou.wang/
+* Description: The first plugin I have created.
+* Version: 1.0
+* Author: Your Name
+* Author URI: https://www.allenyou.wang/
+**/
